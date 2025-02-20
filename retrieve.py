b70baf57ef2a882b13afde3ab719b18f973a85da
The Linh <thelinhboy@gmail.com>
2025-02-20 23:34:24 +0700

    feat/chunk&retrive

--- retrieve.py
+++ retrieve.py
@@ -7,28 +7,27 @@ MODEL_NAME = "intfloat/multilingual-e5-large"  # Model embedding
 
 # ========== RETRIEVE FROM VECTORDB ==========
 def retrieve_from_db(query, db_path, model_name, top_k=1):
-    # Tải embedding model
+    # Load embedding model
     embeddings = HuggingFaceEmbeddings(model_name=model_name)
     
-    # Tải VectorDB từ thư mục đã lưu
+    # Load VectorDB from saved folder
     db = Chroma(persist_directory=db_path, embedding_function=embeddings)
     
-    # Truy xuất các kết quả gần nhất
+    # Retrieve nearest results
     results = db.similarity_search(query, k=top_k)
     return results
 
 # ========== MAIN ==========
 if __name__ == "__main__":
-    # Nhập prompt từ người dùng
+    # Receive prompt from users
     query = "Pipeline Configuration"
     
-    # Thực hiện truy xuất
-    print("⚡ Đang truy xuất dữ liệu từ VectorDB...")
+    # Retrieve
+    print("⚡ Retrieving data from VectorDB...")
     results = retrieve_from_db(query, CHROMA_DB_PATH, MODEL_NAME)
     
-    # In kết quả
-    print("\n💡 Kết quả truy xuất:")
+    # Print result
+    print("\n💡 Retrieval result:")
     for i, result in enumerate(results, start=1):
-        # Lấy tên file từ metadata
-        file_name = result.metadata.get('source', 'Không rõ nguồn')
-        print(f"\n🔸 Kết quả {i} (Sách: {file_name}, Nội dung: {result.page_content})")
+        file_name = result.metadata.get('source', 'Unknown source')
+        print(f"\n🔸 Results {i} (Book: {file_name},\n Content:\n {result.page_content})")
